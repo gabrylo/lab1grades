edf0dfe848e813f5a73fa0e4be4ccd33e637df23
gabrylo <gabrys-0000@wp.pl>
2023-12-06 12:41:55 +0100

    basic

---
+++ lab2phonebase/app/src/main/java/com/example/lab2phonebase/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.lab2phonebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            // Tutaj dodaj obsługę kliknięcia na ikonę trzech kropek
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
